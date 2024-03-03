e99956721660be3cbe74c86c2b9f5c6cef5aade5
skermad <150333566+skermad@users.noreply.github.com>
2024-03-03 19:28:56 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -47,14 +47,20 @@ function addProduct() {
 
     var raw = JSON.stringify([
     {
-        "id": id,
-        "name": name,
-        "description": description,
-        "price": price,
-        "ref": ref,
-        "age": age,
-    }
+        "id": 2,
+        "name": "Ticket to Ride2", "img": [
+            "https://cdn1.philibertnet.com/385325-large_default/ticket-to-ride.jpg"
+          ],
+        "description": "Les Aventuriers du Rail vous invite à bord d'une aventure ferroviaire dans laquelle vous collectez des wago pour prendre le contrôle de chemins de fer reliant les différentes villes des États-Unis d’Amérique.",
+        "price":  "100000",
+        "ref": "DOW7201",
+        "age":  "à partir de 108 ans",
+        "nbPlayers": "2 à 5 joueur(s)",
+        "timing": "30mn à 1h",
+        "video": "https://www.youtube.com/embed/4JhFhyvGdik"
+      }
     ]);
+
 
     var requestOptions = {
     method: 'POST',
